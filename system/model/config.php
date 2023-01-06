8e25ce5e5d37036a15f76e3d7767a26ec95d6d9a
ntluan <luanpkstg294@gmail.com>
2023-01-06 22:59:15 +0700

    test

---
+++ system/model/config.php
@@ -0,0 +1,17 @@
+<?
+$serverdb="localhost";
+$userdb="root";
+$passdb="S@2V5Ey@Mj";
+$namedb="sct_2017";
+
+$visitortimeout=900; //=15 * 60
+
+$adminemail="hotro@127.0.0.1";
+$domain_home="shopcantho.vn/";
+$domain_config="shopcantho.vn/";
+$dir="/"; 
+
+$url="shopcantho.vn/"; 
+date_default_timezone_set("Asia/Ho_Chi_Minh");
+ 
+?>
